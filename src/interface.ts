9db5e91e7844cb84f396a20f018696312017841e
Serhii Zghama <serhii.zghama@edetek.com>
2023-02-11 02:34:32 +0700

    Refactoring code

---
+++ src/interface.ts
@@ -0,0 +1,30 @@
+
+export const USDT = "usdt"
+export interface ITickerPrice {
+    symbol: string;
+    price: number;
+}
+
+export interface ITicker24hr {
+    symbol: string;
+    priceChange: string;
+    priceChangePercent: string;
+    weightedAvgPrice: string;
+    prevClosePrice: string;
+    lastPrice: string;
+    lastQty: string;
+    bidPrice: string;
+    bidQty: string;
+    askPrice: string;
+    askQty: string;
+    openPrice: string;
+    highPrice: string;
+    lowPrice: string;
+    volume: string;
+    quoteVolume: string;
+    openTime: number;
+    closeTime: number;
+    firstId: number;
+    lastId: number;
+    count: number;
+}
